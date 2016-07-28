42f2e00fe46002287bda8888f6deed3b74273b9d
crystalxiaoou <1285949195@qq.com>
2016-07-28 13:31:07 +0800

    commit the pom.xml and java Class and logback.xml

--- logback_init/src/main/java/com/xiaoou/logback/LogbackDemo.java
+++ logback_init/src/main/java/com/xiaoou/logback/LogbackDemo.java
@@ -1,7 +1,26 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
 package com.xiaoou.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Created by baidu on 16/7/27.
+ * Created by xiaoou on 16/7/27.
+ * @author xiaoou
+ * @version 1.0
+ * Logback first demo.
+ *
  */
 public class LogbackDemo {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogbackDemo.class);
+
+    public static void main(String[] args) {
+        LOGGER.trace("======trace");
+        LOGGER.debug("======debug");
+        LOGGER.info("======info");
+        LOGGER.warn("======warn");
+        LOGGER.error("======error");
+    }
 }
